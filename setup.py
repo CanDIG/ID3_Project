d459ae57a3dd9b2a445bb761f9cb29ad9b7fec5a
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-08-11 15:50:55 -0400

    Updated package name and console script name to use hyphens instead of underscores. Removed MANIFEST.in as no data files are being packaged. Removed directories containing data from setup.py as they are not being packaged.

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@ with open('requirements.txt', 'r') as file:
         install_requires[i] = install_requires[i].strip()
 
 setuptools.setup(
-    name='id3_variants_training',
+    name='id3-variants-training',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/CanDIG/ID3_Project',
-    packages=['config', 'release', 'id3_variants_training'],
+    packages=['id3_variants_training'],
     package_dir={'id3_variants_training': 'src/id3_variants_training'},
     python_requires='>=3.7',
     install_requires=install_requires,
-    include_package_data=True,
     entry_points={
         'console_scripts': [
-            'train_id3=id3_variants_training.__main__:main',
+            'train-id3=id3_variants_training.__main__:main',
         ],
     },
 )
