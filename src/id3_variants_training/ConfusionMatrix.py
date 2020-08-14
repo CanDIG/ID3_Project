b3edb8106bcd4e7d1860e13aa39323e0c2ec342e
Samarth-Agarwal1 <agarwalsamarth@hotmail.com>
2020-08-14 16:04:42 -0400

    Samarth agarwal1/refactor (#8)
    * Replaced print_matrix() in ConfusionMatrix with __str__().
    
    * Fixed formatting issues and changed how 2D matrix is created in ConfusionMatrix.__init__().
    
    * Added function get_api() that creates API object. API object is passed in to ConfusionMatrix as a parameter.
    
    * Modified __init__ to only take in API object as parameter and make the api attribute of the class have the api object passed in.
    
    * Modified __init__ to only take in an API object as a parameter and reduced code inside the __init__ function so that duplicate work is not being done. Changed the function names that calculate rates of the confusion matrix and modified the function bodies of them to be cleaner.
    
    * Modified some function names to remove 'rate' from the end of it.

--- src/id3_variants_training/ConfusionMatrix.py
+++ src/id3_variants_training/ConfusionMatrix.py
@@ -1,12 +1,12 @@
 from .ID3_Class import ID3
 from .local_API import LOCAL_API
-from .ID3_Node import ID3_Node
+from .ga4gh_API import GA4GH_API
 
 
 class ConfusionMatrix(ID3):
 
-    def __init__(self, file_path, local):
-        '''
+    def __init__(self, api):
+        """
         Creates confusion matrix with the first index (Y) as the correct population and the
         second index (X) as the predicted population. THe order of the ancestries is dictated
         by the ancestry_list within the api class
@@ -15,81 +15,75 @@ class ConfusionMatrix(ID3):
         predicted is X axis
 
         Args:
-            file_path (str): Path to json file that contains the variant ranges
-            local (bool): Flag to determine whether or not to read locally or from a server
+            api (LOCAL_API | GA4GH_API): API object that is used to interact with the virtual API
 
         Attributes:
-            api (API): API object that is used to interact with the virtual API
+            api (LOCAL_API | GA4GH_API): API object that is used to interact with the virtual API
             root_node (Node): Creates the root node of the tree to be added upon
 
             length (int): length of all the ancestries
-            ancestry_list (list): list of all ancestries without repetitions
             conf_matrix (list): the confusion matrix based on the ID3 classifier
             diagonal_sum (int): sum of the diagonals within the matrix
             total (int): total sum of all values in matrix
-        '''
-        super(ConfusionMatrix, self).__init__(file_path, local)
-
-        # modify API for conf_matrix
-        self.api = LOCAL_API(file_path, conf_matrix=True)
-
-        # initialize ID3 algorithm
-        subset = self.api.get_target_set()
-        self.root_node = ID3_Node('root', subset, True)
-        self.ID3(self.root_node)
+        """
+        super(ConfusionMatrix, self).__init__(api)
 
         # create conf_matrix and calculate useful attributes
         self.length = len(self.api.ancestry_list)
-        self.conf_matrix = [ [0 for x in range(0, self.length)] for y in range(0, self.length) ]
+        self.conf_matrix = [[0] * self.length for _ in range(self.length)]
 
         for variants, popu in zip(self.api.test_variant_list, self.api.test_popu_list):
             include_variants = [self.api.variant_name_list[idx] for idx, is_variant in enumerate(variants) if is_variant == 1]
 
             # Actual Result
-            y = self.api.ancestry_list.index( popu )
+            y = self.api.ancestry_list.index(popu)
             # Predicted Result
-            x = self.api.ancestry_list.index( self.predict(include_variants).most_common_ancestry )
+            x = self.api.ancestry_list.index(self.predict(include_variants).most_common_ancestry)
 
             self.conf_matrix[y][x] += 1
 
-        self.diagonal_sum = sum( [self.conf_matrix[i][i] for i in range(0, self.length)] )
-        self.total = sum( [sum(self.conf_matrix[i]) for i in range(0, self.length)] )
+        self.diagonal_sum = sum([self.conf_matrix[i][i] for i in range(self.length)])
+        self.total = sum([sum(self.conf_matrix[i]) for i in range(self.length)])
 
-    def accuracy(self):
-        '''
+    def get_accuracy(self):
+        """
         How often the classifier is correct
 
         Returns:
             (float): a number between 0 and 1
-        '''
+        """
         return self.diagonal_sum / self.total
 
-    def misclass_rate(self):
-        '''
+    def get_misclassification_rate(self):
+        """
         How often is the classifier wrong
 
         Returns:
             (float): a number between 0 and 1
-        '''
-        return 1 - self.accuracy()
+        """
+        return 1 - self.get_accuracy()
 
-    def true_ancestry_rate(self, ancestry):
-        '''
+    def get_hit_rate(self, ancestry):
+        """
         For a particular ancestry, how often does it predict the correct ancestry
+
+        Args:
+            ancestry (string): a three character code depicting populations of people
 
         Returns:
             (float): a number between 0 and 1
-
-        '''
+        """
         if ancestry not in self.api.ancestry_list:
-            return "Not a valid ancestry"
+            print("Not a valid ancestry")
+            return None
 
         popu_i = self.api.ancestry_list.index(ancestry)
-
         true_anc = self.conf_matrix[popu_i][popu_i]
-        sum_actual_anc = sum([ self.conf_matrix[popu_i][i] for i in range(0, self.length) ])
+        sum_actual_anc = sum(self.conf_matrix[popu_i])
 
-    def false_ancestry_rate(self, ancestry):
+        return true_anc / sum_actual_anc
+
+    def get_miss_rate(self, ancestry):
         """
         For a particular ancestry, how often does it predict the incorrect ancestry
 
@@ -98,12 +92,15 @@ class ConfusionMatrix(ID3):
 
         Returns:
             (float): a number between 0 and 1
-
         """
-        return 1 - self.true_ancestry_rate
+        hit_rate = self.get_hit_rate(ancestry)
+        if not hit_rate:
+            return None
 
-    def precision(self, ancestry):
-        '''
+        return 1 - hit_rate
+
+    def get_precision(self, ancestry):
+        """
         For a particular ancestry, how often is the prediction correct
 
         Args:
@@ -111,18 +108,19 @@ class ConfusionMatrix(ID3):
 
         Returns:
             (float): a number between 0 and 1
-        '''
+        """
         if ancestry not in self.api.ancestry_list:
-            return "Not a valid ancestry"
+            print("Not a valid ancestry")
+            return None
 
         popu_i = self.api.ancestry_list.index(ancestry)
         true_anc = self.conf_matrix[popu_i][popu_i]
-        sum_pred_anc = sum([ self.conf_matrix[i][popu_i] for i in range(0, self.length) ])
+        sum_pred_anc = sum([self.conf_matrix[i][popu_i] for i in range(self.length)])
 
-        return true_anc/sum_pred_anc
+        return true_anc / sum_pred_anc
 
-    def prevalance(self, ancestry):
-        '''
+    def get_prevalence(self, ancestry):
+        """
         How often does the ancestry appear in the sample relative to the sum of all ancestries
 
         Args:
@@ -130,24 +128,22 @@ class ConfusionMatrix(ID3):
 
         Returns:
             (float): a number between 0 and 1
-        '''
+        """
         if ancestry not in self.api.ancestry_list:
-            return "Not a valid ancestry"
+            print("Not a valid ancestry")
+            return None
 
         popu_i = self.api.ancestry_list.index(ancestry)
-        sum_actual_anc = sum([ self.conf_matrix[popu_i][i] for i in range(0, self.length) ])
+        sum_actual_anc = sum(self.conf_matrix[popu_i])
 
         return sum_actual_anc / self.total
 
-    def print_matrix(self):
-        for row in self.conf_matrix:
-            print(row)
+    def __str__(self):
+        return '\n'.join([str(self.conf_matrix[i]) for i in range(len(self.conf_matrix))])
 
 
 if __name__ == "__main__":
     c = ConfusionMatrix()
-    c.print_matrix()
-    print(c.accuracy())
-    print(c.prevalance('ESN'))
-
-
+    print(c)
+    print(c.get_accuracy())
+    print(c.get_prevalence('ESN'))
--- src/id3_variants_training/__main__.py
+++ src/id3_variants_training/__main__.py
@@ -1,4 +1,6 @@
 from .ConfusionMatrix import ConfusionMatrix
+from .local_API import LOCAL_API
+from .ga4gh_API import GA4GH_API
 import argparse
 
 
@@ -9,18 +11,47 @@ def main():
                         help='use remote API to access variant information rather than local VCF files')
     args = parser.parse_args()
 
-    # creates ConfusionMatrix object with a path to the config file and whether to use local VCF files or server VCF files
     use_local_vcf_files = not args.use_candig_apis
-    conf_matrix = ConfusionMatrix(getattr(args, 'config-file'), use_local_vcf_files)
+    config_file_path = getattr(args, 'config-file')
+
+    # get either a LOCAL_API or GA4GH_API object depending on the command line arguments passed in
+    api = get_api(config_file_path, use_local_vcf_files, True)
+
+    conf_matrix = ConfusionMatrix(api)
 
     # prints the ConfusionMatrix
-    conf_matrix.print_matrix()
+    print(conf_matrix)
 
     # prints the list of all the variant names
     print(conf_matrix.api.variant_name_list)
 
     # predicts ancestry of the person with the variant `22:50121766:50121767` and no other variant in `variant_name_list`
     conf_matrix.predict(['22:50121766:50121767'])
+
+
+def get_api(file_path, local, conf_matrix):
+    """
+    Constructs and returns an API object (LOCAL_API or GA4GH_API) depending on the values of <file_path>, <local> and
+    <conf_matrix>.
+
+    :param file_path: path to JSON file that contains the variant ranges
+    :type file_path: str
+    :param local: True if you want to construct an API that reads locally. False if you want to construct an API that
+    reads from a server.
+    :type local: bool
+    :param conf_matrix: True if you want to construct an API for use in a confusion matrix. False otherwise. Note that
+    if the value of this parameter is True, an API that reads locally is constructed regardless of the value of
+    the <local> parameter.
+    :type conf_matrix: bool
+    :return: API object created based on the values of the function parameters
+    :rtype: LOCAL_API | GA4GH_API
+    """
+    if local or conf_matrix:
+        api = LOCAL_API(file_path, conf_matrix)
+    else:
+        api = GA4GH_API(file_path)
+
+    return api
 
 
 if __name__ == '__main__':
