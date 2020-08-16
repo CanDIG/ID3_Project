794dafff768d4152929f3ad7c351a2f5f347ce83
Jonathan Dursi <jonathan@dursi.ca>
2020-08-16 16:19:00 -0400

    pytest tests for simple cases

--- src/id3_variants_training/__predict__.py
+++ src/id3_variants_training/__predict__.py
@@ -4,9 +4,8 @@ import numpy
 from .ConfusionMatrix import ConfusionMatrix
 from .local_API import LOCAL_API
 
-def predict(config_path, model_file):
+def predict(config_path, id3_tree):
     api = LOCAL_API(config_path, False)
-    id3_tree = pickle.load(model_file)
     return ConfusionMatrix(id3_tree, api)
 
 if __name__ == '__main__':
@@ -15,7 +14,8 @@ if __name__ == '__main__':
     parser.add_argument('model_file', help='path to output ID3 file', type=argparse.FileType('rb'))
     args = parser.parse_args()
 
-    conf_matrix = predict(args.config_path, args.model_file)
+    id3_model = pickle.load(args.model_file)
+    conf_matrix = predict(args.config_path, id3_model)
     numpy.set_printoptions(linewidth=10000)
     print(conf_matrix)
     print(conf_matrix.get_accuracy())
--- src/id3_variants_training/__train__.py
+++ src/id3_variants_training/__train__.py
@@ -1,21 +1,17 @@
+import argparse
+import pickle
 from .local_API import LOCAL_API
 from .ga4gh_API import GA4GH_API
 from .ID3_Class import ID3
-import argparse
-import pickle
 
 
-def train(use_local, config_path, model, diagram, verbose=True):
+def train(use_local, config_path, verbose=True):
     if use_local:
         api = LOCAL_API(config_path, False)
     else:
         api = GA4GH_API(config_path)
 
-    id3_tree = ID3(api, verbose)
-    pickle.dump(id3_tree, model)
-    if diagram:
-        id3_tree.print_tree(diagram)
-
+    return ID3(api, verbose)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -28,4 +24,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     use_local_vcf_files = not args.use_candig_apis
     config_file_path = args.config_file
-    train(use_local_vcf_files, config_file_path, args.model_file, args.diagram)
+
+    id3_tree = train(use_local_vcf_files, config_file_path)
+
+    pickle.dump(id3_tree, args.model_file)
+    if args.diagram:
+        id3_tree.print_tree(args.diagram)
+
