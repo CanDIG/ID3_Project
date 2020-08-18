a3c40c4046cfb1ddcf6a745c5fd7f16dd8cf8f64
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-08-18 12:44:16 -0400

    Modified function names in __predict__.py and __train__.py. Updated setuptools console scripts to account for these function name changes.

--- src/id3_variants_training/__predict__.py
+++ src/id3_variants_training/__predict__.py
@@ -5,19 +5,19 @@ from .ConfusionMatrix import ConfusionMatrix
 from .local_API import LOCAL_API
 
 
-def get_confusion_matrix(config_path, id3_tree):
+def predict(config_path, id3_tree):
     api = LOCAL_API(config_path, False)
     return ConfusionMatrix(id3_tree, api)
 
 
-def predict():
+def predict_main():
     parser = argparse.ArgumentParser()
     parser.add_argument('config_path', help='path to the config file that contains vcf file paths', default='config.json')
     parser.add_argument('model_file', help='path to output ID3 file', type=argparse.FileType('rb'))
     args = parser.parse_args()
 
     id3_model = pickle.load(args.model_file)
-    conf_matrix = get_confusion_matrix(args.config_path, id3_model)
+    conf_matrix = predict(args.config_path, id3_model)
     numpy.set_printoptions(linewidth=10000)
     print(conf_matrix)
     print(conf_matrix.get_accuracy())
