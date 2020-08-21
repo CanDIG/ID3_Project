83b0230e73ce68ca41b30afc1c6a81eae1b3999b
Jonathan Dursi <jonathan@dursi.ca>
2020-08-21 15:38:48 -0400

    Finish ped_to_json

--- candig_server/ped_to_json.py
+++ candig_server/ped_to_json.py
@@ -5,21 +5,33 @@ convert ancestry information into a minimal candig-ingest JSON file
 import argparse
 import csv
 import json
+import sys
 
-def ingest_dicts_from_ped(pedfile):
-    patintlist = []
+def dicts_from_ped(pedfile):
+    patientlist = []
     pedreader = csv.DictReader(pedfile, delimiter='\t')
-    for row in pedreader
-Family ID	Individual ID	Paternal ID	Maternal ID	Gender	Phenotype	Population	Relationship	Siblings	Second Order	Third Order	Children	Other Comments
+    for row in pedreader:
+        patid = row["Individual Id"]
+        ethnicity = row["Population"]
+        patient = { "Patient": { "patientId": patid, "ethnicity": ethnicity },
+                    "Sample": { "patientId": patid, "sampleId": f"SAMPLE_{patid}" } }
+        patientlist.append(patient)
+    return patientlist
+
+def patientlist_to_json(patient_list):
+    metadata = {"metadata": patient_list}
+    return json.dumps(metadata)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_ped', help='path to the input .ped file; stdin if not provided',
-            type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('input_ped', help='path to the input .ped file',
+            type=argparse.FileType('r'))
+    parser.add_argument('output_json', help='path to the output json file, stdout if not provided',
+            type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
-
     
+    patient_list = dicts_from_ped(args.input_ped)
+    print(patientlist_to_json(patient_list), file=args.output_json)
     
-
 if __name__ == "__main__":
     main()
